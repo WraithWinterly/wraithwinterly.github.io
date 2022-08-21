d9b867dd8dca18908dcaef4f5a34455038cfc322
WraithWinterly <37941646+WraithWinterly@users.noreply.github.com>
2022-08-20 21:06:16 -0400

    Allow disabling loading icon refresh

--- src/components/LoadingIcon.js
+++ src/components/LoadingIcon.js
@@ -2,14 +2,28 @@ import React, { useRef } from 'react';
 
 import './LoadingIcon.css';
 
-function LoadingIcon({ text, handleRefresh, failed }) {
+function LoadingIcon({ text, handleRefresh, failed, ignoreUser }) {
   const textRef = useRef(null);
   return (
-    <div className='LoadingIcon' onClick={handleRefresh}>
-      <div className="lds-ripple"><div></div><div></div></div>
-      <h3 className={`LoadingIcon-text ${failed ? 'LoadingIcon-text-failed' : ''}`} ref={textRef}>{text + (failed ? ': Error Fetching Data' : '')}</h3>
-    </div>
+    <>
+      {ignoreUser &&
+        <div className='LoadingIcon'>
+          <div className="lds-ripple"><div></div><div></div></div>
+          <pre className={`LoadingIcon-text`}>{text}</pre>
+        </div>
+      }
+      {!ignoreUser &&
+        <div className='LoadingIcon LoadingIcon-with-hover' onClick={handleRefresh}>
+          <div className="lds-ripple"><div></div><div></div></div>
+          <pre className={`LoadingIcon-text ${failed ? 'LoadingIcon-text-failed' : ''}`} ref={textRef}>{text + (failed ? ': Error Fetching Data' : '')}</pre>
+        </div>
+      }
+    </>
   );
 }
+
+LoadingIcon.defaultProps = {
+  ignoreUser: false,
+};
 
 export default LoadingIcon;
