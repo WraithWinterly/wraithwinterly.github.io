631bca78ff375c8b2d0cc65a6d053733d8c3f31c
WraithWinterly <37941646+WraithWinterly@users.noreply.github.com>
2022-09-25 11:33:07 -0400

    Adjust Project Cards on larger screens

--- src/components/page-sections/ProjectCard.js
+++ src/components/page-sections/ProjectCard.js
@@ -1,33 +1,77 @@
 import React, { useId } from 'react';
+import useWindowSize from '../../hooks/useWindowSize';
 
 import './ProjectCard.css';
 
 function ProjectCard({ name, desc, stack, link, linksrc, img }) {
   const id = useId();
-  return (
-    <div className='ProjectCard'>
-      <div className='ProjectCard-text'>
-        <h1 className='accent-color'>{name}</h1>
-        <p className='text-color-light'>{desc}</p>
-      </div>
-      <div className="ProjectCard-image-container-wrapper">
-        <div className='ProjectCard-image-container'>
-          <img className='ProjectCard-image' src={img} alt={name} />
-        </div>
+  const windowSize = useWindowSize();
 
-      </div>
-      <div className='ProjectCard-content'>
-        <a className='button button-large' href={link} target='_blank' rel="noreferrer" >View</a>
-        <a className='button button-large button-github' href={linksrc} target='_blank' rel="noreferrer" >View Source</a>
-      </div>
+  const renderIcons = () => {
+    return (
       <div className='ProjectCard-stack-container'>
         {stack.map((item, index) => {
           return (
-            <img className='ProjectCard-stack-item' key={`${id}-${index}`} src={`https://raw.githubusercontent.com/WraithWinterly/wraithwinterly.github.io/fetch/icons/${item}-icon.png`}></img>
+            <img
+              className='ProjectCard-stack-item'
+              key={`${id}-${index}`}
+              src={`https://raw.githubusercontent.com/WraithWinterly/wraithwinterly.github.io/fetch/icons/${item}-icon.png`}
+            ></img>
           );
         })}
       </div>
-    </div>
+    );
+  };
+
+  return (
+    <>
+      {windowSize.width < 950 && (
+        <div className='ProjectCard'>
+          <div className='ProjectCard-text'>
+            <h1 className='accent-color'>{name}</h1>
+            <p className='text-color-light'>{desc}</p>
+          </div>
+          <div className='ProjectCard-image-container-wrapper'>
+            <div className='ProjectCard-image-container'>
+              <img className='ProjectCard-image' src={img} alt={name} />
+            </div>
+          </div>
+          <div className='ProjectCard-content'>
+            <a className='button button-large' href={link} target='_blank' rel='noreferrer'>
+              View
+            </a>
+            <a className='button button-large button-github' href={linksrc} target='_blank' rel='noreferrer'>
+              View Source
+            </a>
+          </div>
+          {renderIcons()}
+        </div>
+      )}
+      {windowSize.width >= 950 && (
+        <div className='ProjectCard'>
+          <div>
+            <div className='ProjectCard-text'>
+              <h1 className='accent-color'>{name}</h1>
+              <p className='text-color-light'>{desc}</p>
+            </div>
+            <div className='ProjectCard-content'>
+              <a className='button button-large' href={link} target='_blank' rel='noreferrer'>
+                View
+              </a>
+              <a className='button button-large button-github' href={linksrc} target='_blank' rel='noreferrer'>
+                View Source
+              </a>
+            </div>
+          </div>
+          <div className='ProjectCard-image-container-wrapper'>
+            <div className='ProjectCard-image-container'>
+              <img className='ProjectCard-image' src={img} alt={name} />
+            </div>
+            {renderIcons()}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
